0004b054e3e77543503c3b705c8e8db05f7daf25
vinaykr16 <47240832+vinaykr16@users.noreply.github.com>
2019-09-25 12:25:55 +0530

    Update contactform.php

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -6,9 +6,9 @@ $EmailSubject = 'Site contact form';
 $mailheader = "From: ".$_POST["email"]."\r\n"; 
 $mailheader .= "Reply-To: ".$_POST["email"]."\r\n"; 
 $mailheader .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
-$MESSAGE_BODY = "Name: ".$_POST["name"].""; 
-$MESSAGE_BODY .= "Email: ".$_POST["email"].""; 
-$MESSAGE_BODY .= "Mobile_no: ".$_POST["contact_no"].""; 
-$MESSAGE_BODY .= "Message: ".nl2br($_POST["message"]).""; 
+$MESSAGE_BODY = "Name: ".$_POST["name"]."<br>"."Email: ".$_POST["email"]."<br>"."Mobile_no: ".$_POST["contact_no"]."<br>"."Message: ".nl2br($_POST["message"]); 
+// $MESSAGE_BODY .= "Email: ".$_POST["email"].""; 
+// $MESSAGE_BODY .= "Mobile_no: ".$_POST["contact_no"].""; 
+// $MESSAGE_BODY .= "Message: ".nl2br($_POST["message"]).""; 
 mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Failure"); 
 ?>
